206465891e6bf2012dd1718a95e5dc6692805cc5
Jayharer <jay416505@gmail.com>
2021-08-13 11:03:36 +0530

    chore: update requirements.txt

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+import clr, sys, os
+import config
+
+dlllib_path=os.path.join(config.BASE_DIR,"dlllib")
+sys.path.insert(0, dlllib_path)
+
+clr.FindAssembly('BSMAnalytics')
+clr.AddReference('BSMAnalytics')
+from System import Convert
+from System import *
+from BSMAnalytics import ExcelFuncs
+
 from src import routs
--- src/pricer.py
+++ src/pricer.py
@@ -2,14 +2,7 @@ import clr, sys, os
 import config
 from datetime import datetime
 
-dlllib_path=os.path.join(config.BASE_DIR,"dlllib")
-sys.path.insert(0, dlllib_path)
-
-clr.FindAssembly('BSMAnalytics')
-clr.AddReference('BSMAnalytics')
-from System import Convert
-from System import *
-from BSMAnalytics import ExcelFuncs
+from src import ExcelFuncs, Convert
 
 
 class CashEquity:
